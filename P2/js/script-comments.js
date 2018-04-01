b304c23370b463bda3ca20eaf5bc43c9c772d2fa
Ángel Robledillo Perea <angelrobp@gmail.com>
2018-04-01 19:40:37 +0200

    New changes with javascript. Add comments window

---
+++ P2/js/script-comments.js
@@ -0,0 +1,148 @@
+var click = false;
+var insertComment = false;
+var par = true;
+var img = ["../img/user.png","../img/user1.png","../img/user2.png"];
+var contador_img = 1;
+
+var asterisk_censure = "**";
+var bad_words = ["(^|\\s)caca(\\s|$)", "(^|\\s)feo(\\s|$)", "(^|\\s)tonto(\\s|$)", "(^|\\s)idiota(\\s|$)", "(^|\\s)retrasado(\\s|$)", "(^|\\s)nazi(\\s|$)", "(^|\\s)marica(\\s|$)", "(^|\\s)capullo(\\s|$)"];
+var censure = false;
+
+var date = new Date();
+var month = date.getMonth()+1;
+var minute = date.getMinutes();
+if (minute < 10) {
+    minute = "0"+minute;
+}
+
+
+date = date.getDate() + "/" + month + "/" + date.getFullYear() + " " + date.getHours() + ":" + minute;
+function getDate() {
+    return date;
+}
+
+function hasClicked() {
+    click = true;
+}
+
+function clearError () {
+    var error = document.getElementById("error-message");
+    error.style.display = "none";
+    error.innerHTML = " ";
+}
+
+function reviser(submit) {
+    censure = false;
+    var x = document.getElementById("input-comentario");
+    var str = x.value;
+    var res = str.charAt(str.length-1);
+
+    if (res===" " || submit) {
+        // RegEx sirve para definir una expresion regular
+        // "i" es para ignorar un caso y "g" para un ambito global,
+        // "|" para un operador OR
+        var regex = new RegExp(bad_words.join("|"), "gi");
+        x.value = str.replace(regex, function (match) {
+            //reemplazamos cada letra por un *
+            var censured = '';
+            var i = 0;
+
+            if (match.charAt(0) === ' ') {
+                censured += ' ';
+                i=1;
+            }
+
+            for (i; i < match.length-1; i++) {
+                censured += '*';
+            }
+
+            if (match.charAt( match.length-1) === '' || match.charAt( match.length-1) === ' ') {
+                censured += ' ';
+            }
+            else {
+                censured += '*';
+            }
+
+            return censured;
+        });
+
+        if (x.value.indexOf(asterisk_censure) > -1) {
+            censure = true;
+        }
+    }
+}
+
+/*
+  Se sobreescribe el metodo submit para que no se recargue la pagina
+*/
+
+document.getElementById("form-messages").onsubmit = function () {
+    reviser(true);
+    if (!censure) {
+        var div = document.createElement('div');
+        var h = document.forms["form-messages"]["input-comentario"].value;
+        var date = getDate();
+
+        if (par) {
+            div.className = 'comentario-usuario par';
+            par=false;
+        }
+        else {
+            div.className = 'comentario-usuario';
+            par=true;
+        }
+
+
+        var html= '<div class="mensaje-user"><div class="user-img"><div class="nombre-usuario">';
+        html += document.forms["form-messages"]["input-nombre"].value +'</div><img src="';
+        html += img[contador_img]+'"></div><div class="texto-user"><p>';
+        html += document.forms["form-messages"]["input-comentario"].value +'</p></div></div><span class="time-right">'+ getDate() +'</span>';
+        div.innerHTML = html;
+
+        var comentarios = document.getElementById('comentarios-anteriores');
+        comentarios.insertBefore(div, comentarios.childNodes[0]);
+
+        if (contador_img === 2) {
+            contador_img = 0;
+        }
+        else {
+            contador_img += 1;
+        }
+
+        document.forms["form-messages"]["input-comentario"].value = "";
+        document.forms["form-messages"]["input-nombre"].value = "";
+        document.forms["form-messages"]["input-email"].value = "";
+    }
+    else {
+        var error = document.getElementById("error-message");
+        error.style.display = "initial";
+        error.innerHTML = "*El comentario contiene contenido censurado*";
+    }
+
+    return false;
+};
+
+document.getElementById( 'contenedor-comentarios' ).addEventListener( 'click', function() {
+
+    var x = document.getElementById("comentarios");
+    if (this.style.width == '4%' || this.style.width == '') {
+        this.style.width = '24%';
+        x.style.display = "initial";
+        x = document.getElementsByClassName("imagen-comentarios")
+        x[0].style.display = "none";
+        x = document.getElementById("informacion");
+        x.style.width = "59%";
+    }
+
+    if (click) {
+        this.style.width = '4%';
+        x = document.getElementById("comentarios");
+        x.style.display = 'none';
+        x = document.getElementById("informacion");
+        x.style.width = '79%';
+        x = document.getElementsByClassName("imagen-comentarios");
+        x[0].style.display = 'initial';
+        click=false;
+    }
+
+}, false );
